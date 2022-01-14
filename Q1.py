090c094c7dea3625e1134ad2343d23a7550ded3b
shineoh <rhkdgnl2054@gmail.com>
2022-01-14 22:52:04 +0900

    2번 추가

--- Q1.py
+++ Q1.py
@@ -1,3 +1,7 @@
-# Q1. Hello World!를 출력하시오.
+# D1. 1단계 입출력과 사칙연산
+# 
+# Q1.  Hello World!를 출력하시오.
+print("Hello World!")
 
-print('Hello World!')
+# Q2. 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+print("강한친구 대한육군\n강한친구 대한육군")
