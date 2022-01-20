d77f6c50fbebaeda6fe4ad90e31995d7451dc44b
shineoh <rhkdgnl2054@gmail.com>
2022-01-20 12:04:13 +0900

    3단계 Q6~Q8

--- 백준/step3_2.py
+++ 백준/step3_2.py
@@ -8,3 +8,16 @@
 N = int(input())
 for i in range(N,0,-1):
     print(i)
+
+#Q7. #11021 A+B-7
+N = int(input())
+for i in range(1,N+1):
+    a,b = map(int, input().split())
+    print(f'Case #{i}: {a+b}')
+
+#Q8. #11022 A+B-8
+N = int(input())
+for i in range(1,N+1):
+    a,b = map(int, input().split())
+    print(f'Case #{i}: {a} + {b} = {a+b}')
+
