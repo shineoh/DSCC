b5dff6b78772dbd34541a27490424bd2bf0943cf
shineoh <rhkdgnl2054@gmail.com>
2022-01-29 17:28:36 +0900

    boj step7 ~Q7

--- 백준/step7_3.py
+++ 백준/step7_3.py
@@ -1,4 +1,21 @@
 #Q6 #1152 단어의 개수
-words = input().split()
-lst = list(words)
-print(len(lst))
+# words = input().split()
+# lst = list(words)
+# print(len(lst))
+
+
+#Q7 #2908 상수
+a, b = input().split()
+a_re = ''
+b_re = ''
+
+for i in range(len(a)):
+    a_re+=a[len(a)-1-i]
+
+for i in range(len(b)):
+    b_re+=b[len(a)-1-i]
+
+if a_re > b_re:
+    print(a_re)
+else:
+    print(b_re)
