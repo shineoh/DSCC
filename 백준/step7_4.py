22401754d71998ce04a4f02698743b60b8178e59
shineoh <rhkdgnl2054@gmail.com>
2022-01-30 22:15:26 +0900

    boj step7 finish

--- 백준/step7_4.py
+++ 백준/step7_4.py
@@ -1,12 +1,26 @@
 #Q9 #2941 크로아티아 알파벳.
 
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
+# croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+# word = input()
 
-for i in croatia :
-    word = word.replace(i, '*')  # input 변수와 동일한 이름의 변수
-print(len(word))
+# for i in croatia :
+#     word = word.replace(i, '*')  # input 변수와 동일한 이름의 변수
+# print(len(word))
 
 
 
-#
+#Q10 #1316 그룹 단어 체크
+n = int(input())
+
+group_word = 0
+error = 0
+for i in range(n):
+    word = input()
+    for index in range(len(word)-1):  # 인덱스 범위 생성 : 0부터 단어개수 -1까지 
+        if word[index] != word[index+1]:  # 연달은 두 문자가 다른 때,
+            new_word = word[index+1:]  # 현재글자 이후 문자열을 새로운 단어로 생성
+            if new_word.count(word[index]) > 0:  # 남은 문자열에서 현재글자가 있있다면
+                error += 1  # error에 1씩 증가.
+                break
+ans = n-error
+print(ans)
