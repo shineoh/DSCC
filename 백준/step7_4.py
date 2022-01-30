931273a917ea981b6faf916d7914c08cc5b4bc8a
shineoh <rhkdgnl2054@gmail.com>
2022-01-30 22:00:51 +0900

    boj step7 Q9

---
+++ 백준/step7_4.py
@@ -0,0 +1,12 @@
+#Q9 #2941 크로아티아 알파벳.
+
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia :
+    word = word.replace(i, '*')  # input 변수와 동일한 이름의 변수
+print(len(word))
+
+
+
+#
