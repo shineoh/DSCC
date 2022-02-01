47e9e1689d2987d9a1297225e9e58e20a0c81077
shineoh <rhkdgnl2054@gmail.com>
2022-02-01 14:13:02 +0900

    boj step8 Q1~Q2

--- 백준/step8_1.py
+++ 백준/step8_1.py
@@ -1,14 +1,23 @@
 #Q1 #1712 손익분기점
 
-fixed_cost, variable_cost, sales_price = map(int,input().split())
+# fixed_cost, variable_cost, sales_price = map(int,input().split())
 
-def break_even_point():
-    cnt = 0
-    if variable_cost >= sales_price:
-        cnt = -1
-        return cnt
-    else:
-        cnt = (fixed_cost // (sales_price-variable_cost)) + 1
-    return cnt 
+# def break_even_point():
+#     cnt = 0
+#     if variable_cost >= sales_price:
+#         cnt = -1
+#         return cnt
+#     else:
+#         cnt = (fixed_cost // (sales_price-variable_cost)) + 1
+#     return cnt 
 
-print(break_even_point())
+# print(break_even_point())
+
+#Q2 #2292 벌집
+
+n = int(input())
+ans=1
+while n>1:
+    n=n-6*ans
+    ans+=1
+print(ans)
