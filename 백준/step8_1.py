62bd74f18d39dfce8c0622c94ee048d59d3adc0c
shineoh <rhkdgnl2054@gmail.com>
2022-02-02 23:27:52 +0900

    boj step8 Q1~Q3

--- 백준/step8_1.py
+++ 백준/step8_1.py
@@ -21,3 +21,20 @@ while n>1:
     n=n-6*ans
     ans+=1
 print(ans)
+
+#q3 #1193 분수찾기
+X=int(input())
+
+line=1
+while X>line:
+    X-=line
+    line+=1
+    
+if line%2==0:
+    a=X
+    b=line-X+1
+else:
+    a=line-X+1
+    b=X
+    
+print(a, '/', b, sep='')
