b9ac8b803c1cadc814d2e9613dfa5be0e253e9f0
shineoh <rhkdgnl2054@gmail.com>
2022-02-04 23:40:55 +0900

    설탕 배달부

---
+++ 백준/step8_기본수학1/8_7.py
@@ -0,0 +1,14 @@
+#2839 #설탕배달
+
+sugar = int(input())
+
+bag = 0
+while sugar >= 0 :
+    if sugar % 5 == 0 :  # 5의 배수이면
+        bag += (sugar // 5)  # 5로 나눈 몫을 구해서 정수 만들기
+        print(bag)
+        break
+    sugar -= 3  
+    bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
+else :
+    print(-1)
