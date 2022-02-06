423469656cae081cbe277bec945fea957196a53d
shineoh <rhkdgnl2054@gmail.com>
2022-02-06 23:56:10 +0900

    조건확인 잘하기! 코드 간단히 짜기!

--- 백준/step9_기본수학2/9_3.py
+++ 백준/step9_기본수학2/9_3.py
@@ -1,23 +1,11 @@
 #11653 #소인수분해
 
-def is_prime_num(num):
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
-
 n = int(input())
-while True:
-    x = 2
-    if is_prime_num(n):
-        print(n)
-        break
-
-    while True:
-        if n % x == 0:
-            n = n // x
-            print(x)
-            break
-        else:
-            x += 1
+x = 2
+while n!=1:
+    if n % x == 0:
+        n = n // x
+        print(x)
+    else:
+        x += 1
     
