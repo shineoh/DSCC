db4ccfddc016e18832f819e0cef593408d43153d
shineoh <rhkdgnl2054@gmail.com>
2022-02-06 23:32:29 +0900

    두고봐,, 백트래킹 배우고 보자

--- 백준/step9_기본수학2/9_2.py
+++ 백준/step9_기본수학2/9_2.py
@@ -17,9 +17,10 @@ for num in range(m, n+1):
 
 if lst == []:
     print(-1)
-
 else:
-    ans_1 = sum(lst)
-    ans_2= min(lst)
+    ans_2= 100
+    for number in lst:
+        if number < ans_2:
+            ans_2 = number
     print(ans_1)
     print(ans_2)
