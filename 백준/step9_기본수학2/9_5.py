8a981543da29fc32f136323ad7d641c49e57ad98
shineoh <rhkdgnl2054@gmail.com>
2022-02-07 23:03:53 +0900

    시간초과의 늪

---
+++ 백준/step9_기본수학2/9_5.py
@@ -0,0 +1,24 @@
+#4948 #베트르랑 공준
+
+
+##시간초과
+# while True:
+#     n = int(input())
+#     if n==0:
+#         break
+#     cnt = 0
+#     for i in range(n+1, (2*n)+1) :
+#         if i == 2:
+#             cnt+=1
+
+#         else:
+#             for j in range(2, int(((2*n)**0.5))+1):
+#                 if i % j == 0:
+#                     break
+#             else:
+#                 cnt+=1
+#     print(cnt)
+
+
+
+
