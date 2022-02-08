c8ac68fdd3e390352d32eb9b10cccfa023ea2b15
shineoh <rhkdgnl2054@gmail.com>
2022-02-09 00:07:26 +0900

    좌표 문제 코드줄이기

--- 백준/step9_기본수학2/9_8.py
+++ 백준/step9_기본수학2/9_8.py
@@ -23,3 +23,14 @@ elif y_lst[0] == y_lst[2]:
     ans_y = y_lst[1]
 
 print(f'{ans_x} {ans_y}')
+
+
+## 코드 줄이기 with Jay
+
+for i in range(3):
+    x_lst.append(x_lst[i])
+    y_lst.append(y_lst[i])
+    if x_lst[i] == x_lst[i+1]:
+        ans_x = x_lst[i+2]
+    if y_lst[i] == y_lst[i+1]:
+        ans_y = y_lst[i+2]
