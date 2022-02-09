c674a4ef5ac0495e52e071280d39889dd82e65ea
shineoh <rhkdgnl2054@gmail.com>
2022-02-09 23:54:38 +0900

    1249_보급로 보급이 끊겼답니다git add .

--- swea/1249_보급로.py
+++ swea/1249_보급로.py
@@ -2,3 +2,12 @@
 #swea 1249 보급로
 #2022-02-09
 
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    lst = []*N
+    for i in range(N):
+        for j in range(N):
+            lst[i].append
+    
+
