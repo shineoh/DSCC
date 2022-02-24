3c0abc47f2a9bf8d41cb8353c14fc254fb80067f
shineoh <rhkdgnl2054@gmail.com>
2022-02-24 23:48:36 +0900

    boj 10828 인풋을 빨리 받기 위한 노력도 필요해

--- 백준/step18_스택/10828_스택.py
+++ 백준/step18_스택/10828_스택.py
@@ -1,21 +1,26 @@
-T = int(input())
+
+import sys
+
+T = int(sys.stdin.readline())
 stack = []
 for _ in range(1, T+1):
-    c = input()
-    if c == 'top':
-        print(stack[-1])
-    elif c == 'size':
+    c = sys.stdin.readline().split()
+    if c[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+    elif c[0] == 'size':
         print(len(stack))
-    elif c == 'empty':
+    elif c[0] == 'empty':
         if stack:
             print(0)
         else:
             print(1)
-    elif c == 'pop':
+    elif c[0] == 'pop':
         if stack:
             print(stack.pop())
         else:
             print(-1)
     else:
-        a, b = c.split()
-        stack.append(int(b))
+        stack.append(int(c[1]))
