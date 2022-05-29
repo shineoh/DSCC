f7a8d6a8305bc7137322b99b8f962aac9e0dcba3
shineoh <rhkdgnl2054@gmail.com>
2022-05-29 22:41:50 +0900

    SQL string,date 1~2

---
+++ 프로그래머스/SQL/String_Date.sql
@@ -0,0 +1,11 @@
+-- 루시와 엘라 찾기
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID
+
+-- 이름에 el이 들어가는 동물 찾기
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Dog' AND NAME LIKE '%EL%'
+ORDER BY NAME
