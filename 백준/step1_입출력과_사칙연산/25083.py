2e80194b68b6121bb32ebe70123969ecc67d6ca7
shineoh <rhkdgnl2054@gmail.com>
2022-06-09 14:02:15 +0900

    새싹아 무럭무럭 자라라

---
+++ 백준/step1_입출력과_사칙연산/25083.py
@@ -0,0 +1,10 @@
+# 25083_새싹
+
+def leaf():
+   print('         ,r\'\"7')
+   print('r`-_   ,\'  ,/')
+   print(' \. \". L_r\'')
+   print('   `~\/')
+   print('      |')
+   print('      |')
+leaf()
